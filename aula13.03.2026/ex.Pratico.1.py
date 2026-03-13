bf10f9db33f1d00968a9079999c6c2fb81212676
Th1ago-V <thiagomacielmoura2009@gmail.com>
2026-03-13 08:53:47 -0300

    exercicio preparatorio para prova

---
+++ aula13.03.2026/ex.Pratico.1.py
@@ -0,0 +1,52 @@
+from types import DynamicClassAttribute
+import math
+
+class Pessoa:
+  def __init__(self, nome):
+    self.nome = nome
+
+class Aluno(Pessoa):
+  def __init__(self,nome, prontuario):
+    self.nome = nome
+    self.prontuario = prontuario
+
+class Disciplina:
+  def __init__(self, dnome, dprof, dnum, dnota, dtrab, dmedia, dsitu):
+    self.dnome = dnome
+    self.dprof = dprof
+    self.dnum = dnum
+    self.dnota = dnota
+    self.dtrab = dtrab
+    self.dmedia = dmedia
+    self.dsitu = dsitu
+
+class DisciplinaReg(Disciplina):
+    def CalcMed(self):
+      self.dmedia = (self.dnota + self.dtrab)/2
+      self.dmedia = math.ceil(self.dmedia)
+      if self.dmedia >= 7.0:
+        print("Aprovado com média", f"{self.dmedia:.2f}", "\n") 
+      elif 5 < self.dmedia < 6.9 :
+        print("Recuperação. com média", f"{self.dmedia:.2f}", "\n")
+      else:
+        print("Reprovado com média", f"{self.dmedia:.2f}", "\n")
+
+print("=======================")
+print("RELATÓRIO DO ALUNO")
+print("=======================")
+
+aluno = Aluno(input("Entre com o nome do aluno: "),input("Entre com o prontuário do aluno: "))
+
+disp = [] 
+
+print("\n")
+for i in range(4):
+  disciplina = DisciplinaReg(input("Entre com a disciplina: "),
+      input("Entre com o professor: "),
+      float(input("Entre com o número de aulas: ")), 
+      int(input("Entre com a nota da prova: ")),
+      int(input("Entre com a nota do trabalho: ")),
+      0.0, 
+      "neutra")
+  
+  disciplina.CalcMed()
